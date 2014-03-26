ea132d1fd179c83355a661bb650d15b19074dc02
Ben Short <benshort.mouse@gmail.com>
2014-03-26 11:24:08 +0000

    nearly solved the height issue

--- js/main.js
+++ js/main.js
@@ -1,8 +1,23 @@
 $(document).ready( function(){
-	// handleMenuHeight();
+	handleContentHeight();
 })
 
-function handleMenuHeight(){
-	var windowHeight = $(window.top).height();
-	$('.content').css({minHeight: windowHeight})
+function handleContentHeight(){
+	var windowHeight = $(window).height();
+	var documentHeight = $(document).height();
+	var footerHeight = $('.footer').height();
+	var headerHeight = $('.header').height();
+	var viewableHeight = windowHeight - (footerHeight + headerHeight);
+
+	//if the content is longer than the minimum viewing height don't set it to the viewable height
+	if(documentHeight > viewableHeight)
+	{
+		console.log('bigger than'+viewableHeight);
+		$('.content').height(documentHeight);
+	}
+	else
+	{
+		console.log('set to '+documentHeight);
+		$('.content').height(viewableHeight);
+	}
 }
